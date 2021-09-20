0aa07e09257ffd36b4c4d9c0e2bc37475c876d52
Paulo Calanca <sir.calanca@gmail.com>
2021-09-20 17:52:56 -0300

    final-poluido

--- Billboard200/spiders/billboard.py
+++ Billboard200/spiders/billboard.py
@@ -1,6 +1,6 @@
 import scrapy
-import time
-from datetime import date, datetime, timedelta
+import json
+import pandas as pd
 
 class BillboardSpider(scrapy.Spider):
     name = 'billboard'
@@ -13,22 +13,36 @@ class BillboardSpider(scrapy.Spider):
         for url in self.start_urls:
             yield scrapy.Request(url, headers=self.headers)
     
-    
+        
     def parse(self, response):
      
         hits = response.xpath("//li[starts-with(@class, 'chart-list__element')]")
-        for hit in hits:
+        scrapy.Request(url='https://www.billboard.com/charts/billboard-200/', headers=self.headers, callback=self.parse)
+        df = pd.DataFrame()
+        posicao = response.xpath(".//span[@class='chart-element__rank__number']/text()").extract()
+        df['posicao'] = posicao
+        titulo = response.xpath(".//span[starts-with(@class, 'chart-element__information__song')]/text()").extract()
+        df['song_name'] = titulo
+        artista = response.xpath(".//span[starts-with(@class, 'chart-element__information__artist')]/text()").extract()
+        df['artista'] = artista
+        pos_ultima_semana = response.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[0].extract()
+        df['ultima_semana'] = pos_ultima_semana
+        pico = response.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[1].extract()
+        df['pico'] = pico
+        semanas_no_topo = response.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[2].extract()
+        df['semanas'] = semanas_no_topo
+        df.to_csv (r'C:\Users\SrCal\Desktop\Aula_IA\Aula_raspagem\Projetos\Billboard200\billboard.csv', index = None, header=True)
+        
+        
+        #if not os.path.exists(self.save_dir):
+         #   os.makedirs(self.save_dir)
+        #billboard = os.path.join(self.save_dir, 'billboard.csv')
+        #df.to_csv(billboard, index=False)
+            #print(titulo,artista,posicao,ultima_semana,pico,semanas)
+            
 
-            posicao = response.xpath(".//span[@class='chart-element__rank__number']/text()").get(),
-            titulo = response.xpath(".//span[starts-with(@class, 'chart-element__information__song')]/text()").get(),
-            artista = response.xpath(".//span[starts-with(@class, 'chart-element__information__artist')]/text()").get(),
-            ultima_semana = response.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[0].get(),
-            pico = response.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[1].get(),
-            semanas = response.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[2].get()
-            print(titulo,artista,posicao,ultima_semana,pico,semanas)
-     
-
-     
+       # except IndexError as err:
+        #    print("Deu merda".format(err))
      
       #  hot200 = response.xpath("//li[starts-with(@class, 'chart-list__element')]")
         
@@ -38,7 +52,16 @@ class BillboardSpider(scrapy.Spider):
        # ultima_semana = response.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[0].get(),
        # pico = response.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[1].get(),
        # semanas = response.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[2].get()
-    
+      #  for hit in hits:
+      #      yield {
+      #          'titulo': hit.xpath(".//span[starts-with(@class, 'chart-element__information__song')]/text()").get(),
+      #          'artista': hit.xpath(".//span[starts-with(@class, 'chart-element__information__artist')]/text()").get(),
+      #          'posicao': hit.xpath(".//span[@class='chart-element__rank__number']/text()").get(),
+      #          'ultima_semana': hit.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[0].get(),
+      #          'pico': hit.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[1].get(),
+       #         'semanas': hit.xpath(".//span[contains(@class, 'chart-element__meta ')]/text()")[2].get()
+       #     }
+    #    yield 
       #  yield {   
        #     'titulo': titulo,
        #     'artista' :artista,
